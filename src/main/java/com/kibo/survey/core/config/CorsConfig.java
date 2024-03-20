6187f1333878496f53d5a365d56a7ef263854622
yusssss <yusuf.acmaci@hotmail.com>
2024-03-20 16:42:04 +0300

    cors

--- src/main/java/com/kibo/survey/core/config/CorsConfig.java
+++ src/main/java/com/kibo/survey/core/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://survey-frontend-opal.vercel.app","http://survey-frontend-opal.vercel.app", "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders(
                                 "Access-Control-Allow-Headers",
